f52da15a7c06bb98cbb629f8c44854204686350d
catsymptote <catsymptote@gmail.com>
2018-04-30 10:58:02 +0200

    Variablelized the values.

--- main.cpp
+++ main.cpp
@@ -10,17 +10,25 @@
 int main()
 {
     std::cout << "Running..." << std::endl;
-    int width   = 10000;
-    int height  = 10000;
-    //mathFunction zFunc(100000, width, height, -0.750045367143 -0.000000001, 0.004786271734 -0.000000001, 0.000000002, 0.000000002);    // avg 72k iters per pixel :|   http://colinlmiller.com/fractals/gallery.htm
-    mathFunction zFunc(100, width, height, -2, -2, 4, 4);
+    /// Set variables
+    int imageWidth      = 1024;
+    int imageHeight     = 1024;
+    int maxIterations   = 128;
+    double fieldMinX    = -2;
+    double fieldMinY    = -2;
+    double fieldSizeX   = 4;
+    double fieldSizeY   = 4;
+
+    /// Construct the Z-function and run its plane iterator.
+    //mathFunction zFunc(100000, imageWidth, imageHeight, -0.750045367143 -0.000000001, 0.004786271734 -0.000000001, 0.000000002, 0.000000002);    // avg 72k iters per pixel :|   http://colinlmiller.com/fractals/gallery.htm
+    mathFunction zFunc(maxIterations, imageWidth, imageHeight, fieldMinX, fieldMinY, fieldSizeX, fieldSizeY);
     zFunc.planeIterator();
     std::cout << "z function complete" << std::endl;
     std::vector<std::vector<int> > iterPlane = zFunc.getIterPlane();
 
-    imageHandler imag(iterPlane, "img/img.ppm", "P3", width, height, 255);
+    /// Construct and make the PPM image of the field.
+    imageHandler imag(iterPlane, "img/img.ppm", "P3", imageWidth, imageHeight, 255);
     std::cout << "File creation initialized" << std::endl;
-
     imag.makeImgFile();
 
     std::cout << "Finished!" << std::endl;
